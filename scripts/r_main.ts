3b163454a8fe772044f4d4af436ad039d5622982
Mitsu Takeuchi <mitsu.take.gemini@gmail.com>
2021-12-06 23:49:35 +0900

    fix: 不要なコードを削除

--- scripts/r_main.ts
+++ scripts/r_main.ts
@@ -1,6 +1,5 @@
 import "babel-polyfill";
 import "../tags/App";
-import { run } from "./util/lazy";
 
 if ("serviceWorker" in navigator && process.env.NODE_ENV !== "desktop") {
   navigator.serviceWorker.register(
@@ -8,10 +7,3 @@ if ("serviceWorker" in navigator && process.env.NODE_ENV !== "desktop") {
     { type: "module" }
   );
 }
-
-run(async () => {
-  // const { albumIdFromArg = null } = await albumManager.init();
-  // if (albumIdFromArg) {
-  //   openAlbum(albumIdFromArg);
-  // }
-});
